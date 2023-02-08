20a2305a81022cec7da799d11e634695d94d5a6d
Serenata2 <astg98@gmail.com>
2023-02-08 10:43:35 +0900

    리스너를 이용해서 Apriltag Detection한 후 사각형 그리기 구현

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -14,14 +14,22 @@ Java_com_example_apriltagapp_CameraActivity_ConvertRGBtoGray(JNIEnv *env, jobjec
     cvtColor(matInput, matResult, COLOR_RGBA2GRAY);
 }
 
-// 미래에 개선할 것
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_example_apriltagapp_CameraActivity_DrawRectangle(JNIEnv *env, jobject thiz,
                                                              jlong mat_addr_input,
-                                                             jlong mat_addr_result) {
+                                                             jdoubleArray arr) {
     Mat &matInput = *(Mat *)mat_addr_input;
-    Mat &matResult = *(Mat *)mat_addr_result;
 
-    cvtColor(matInput, matResult, COLOR_RGBA2GRAY);
+    jdouble* arr2 = (*env).GetDoubleArrayElements( arr, NULL);
+    Point_ <int> pts1[4] = {Point((int)arr2[0], (int)arr2[1]), Point((int)arr2[2], (int)arr2[3]),
+                      Point((int)arr2[4], (int)arr2[5]), Point((int)arr2[6], (int)arr2[7])};
+
+    const Point* pts = pts1;
+    int npts = 4;
+
+    cv::polylines(matInput, &pts, &npts, 1, true, Scalar(255.0, 0.0, 0.0), 2);
+
+    //line(matInput, Point((int)arr2[0], (int)arr2[1]), Point((int)arr2[2], (int)arr2[3]), Scalar(255.0, 0.0, 0.0), 20);
 }
