e99607e0806992c3e41bc07e63a7f48c34d9e4d8
hhtboy <59467684+hhtboy@users.noreply.github.com>
2023-02-08 10:46:36 +0900

    native 코드 위치 수정

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,23 +3,22 @@
 
 using namespace cv;
 
+
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_example_apriltagapp_CameraActivity_ConvertRGBtoGray(JNIEnv *env, jobject thiz,
-                                                             jlong mat_addr_input,
-                                                             jlong mat_addr_result) {
+Java_com_example_apriltagapp_MainActivity_00024Companion_convertRGBtoGray(JNIEnv *env, jobject thiz,
+                                                                          jlong mat_addr_input,
+                                                                          jlong mat_addr_result) {
     Mat &matInput = *(Mat *)mat_addr_input;
     Mat &matResult = *(Mat *)mat_addr_result;
 
     cvtColor(matInput, matResult, COLOR_RGBA2GRAY);
 }
-
-// 미래에 개선할 것
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_example_apriltagapp_CameraActivity_DrawRectangle(JNIEnv *env, jobject thiz,
-                                                             jlong mat_addr_input,
-                                                             jlong mat_addr_result) {
+Java_com_example_apriltagapp_MainActivity_00024Companion_drawRectangle(JNIEnv *env, jobject thiz,
+                                                                       jlong mat_addr_input,
+                                                                       jlong mat_addr_result) {
     Mat &matInput = *(Mat *)mat_addr_input;
     Mat &matResult = *(Mat *)mat_addr_result;
 
