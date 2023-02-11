8f84316044fe346414805c25d649da037807819c
Serenata2 <astg98@gmail.com>
2023-02-11 23:44:18 +0900

    native-lib관련 코드들 리펙토링 진행

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -16,147 +16,94 @@ Java_com_example_apriltagapp_view_camera_CameraFragment_ConvertRGBtoGray(JNIEnv 
     cvtColor(matInput, matResult, COLOR_RGBA2GRAY);
 }
 
+/*
+ * Class:     Java_apriltag_OpenCVNative
+ * Method:    draw_polylines
+ * openCV polyline을 그립니다.
+ */
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_example_apriltagapp_view_camera_CameraFragment_DrawRectangle(JNIEnv *env, jobject thiz,
-                                                                      jlong mat_addr_input,
-                                                                      jdoubleArray arr) {
+Java_apriltag_OpenCVNative_draw_1polylines(JNIEnv *env, jclass clazz, jlong mat_addr_input,
+                                           jdoubleArray arr) {
     Mat &matInput = *(Mat *)mat_addr_input;
+    jdouble* jni_arr = (*env).GetDoubleArrayElements(arr, NULL);
+    int len = (*env).GetArrayLength(arr);
 
-    jdouble* arr2 = (*env).GetDoubleArrayElements( arr, NULL);
-    Point_ <int> pts1[4] = {Point((int)arr2[0], (int)arr2[1]), Point((int)arr2[2], (int)arr2[3]),
-                            Point((int)arr2[4], (int)arr2[5]), Point((int)arr2[6], (int)arr2[7])};
+    // polyline을 그립니다.
+    vector<Point2i> vector_pts;
+    for(int i = 0; i < len; i++) {
+        vector_pts.emplace_back(jni_arr[2*i], jni_arr[2*i + 1]);
+    }
+    polylines(matInput, vector_pts, true, Scalar(255.0, 0.0, 0.0), 20);
 
-    const Point* pts = pts1;
-    int npts = 4;
-
-    std::vector<Point2i> vector_pts;
-    vector_pts.push_back(Point((int)arr2[0], (int)arr2[1]));
-    vector_pts.push_back(Point((int)arr2[2], (int)arr2[3]));
-    vector_pts.push_back(Point((int)arr2[4], (int)arr2[5]));
-    vector_pts.push_back(Point((int)arr2[6], (int)arr2[7]));
-
-    polylines(matInput, vector_pts, true, Scalar(255.0, 0.0, 0.0), 2);
+    (*env).ReleaseDoubleArrayElements(arr, jni_arr, 0);
 }
 
-// apriltag 위에 openCV로 화살표 그리는 함수
+/*
+ * Class:     Java_apriltag_OpenCVNative
+ * Method:    draw_polylines_on_apriltag
+ * Signature: cv::solvePnP, cv::projectPoints
+ * apriltag 위에 openCV polyline을 그립니다.
+ */
 extern "C"
 JNIEXPORT void JNICALL
-Java_apriltag_ApriltagDrawNative_DrawArrow(JNIEnv *env, jclass thiz,
-                                                               jlong mat_addr_input,
-                                                               jdoubleArray arr,
-                                                               jdoubleArray drawArray) {
+Java_apriltag_OpenCVNative_draw_1polylines_1on_1apriltag(JNIEnv *env, jclass clazz,
+                                           jlong mat_addr_input,
+                                           jdoubleArray arr,
+                                           jdoubleArray drawArray) {
     Mat &matInput = *(Mat *)mat_addr_input;
     jdouble* jni_arr = (*env).GetDoubleArrayElements(arr, NULL);
     jdouble* jni_draw_array = (*env).GetDoubleArrayElements(drawArray, NULL);
     int len = (*env).GetArrayLength(drawArray);
 
+    /*
+    cameraMatrix : (3 x 3)   fx, 0,  cx
+                             0,  fy, cy
+                             0,  0,  1
+    */
     double data[9] = { 3156.71852, 0, 359.097908,
                        0, 3129.52242, 239.736909,
                        0, 0, 1};
     Mat cameraM = Mat(3, 3, CV_64FC1, data);
-    Mat distortionC = Mat::zeros(5, 1, CV_64FC1);
+    Mat distortionC = Mat::zeros(5, 1, CV_64FC1); // 왜곡 계수
 
-    vector<Point3f> objectPoint;
     vector<Point2f> imagePoint;
+    imagePoint.emplace_back((float) jni_arr[0], (float)jni_arr[1]);
+    imagePoint.emplace_back((float) jni_arr[2], (float)jni_arr[3]);
+    imagePoint.emplace_back((float) jni_arr[4], (float)jni_arr[5]);
+    imagePoint.emplace_back((float) jni_arr[6], (float)jni_arr[7]);
 
-    imagePoint.push_back(Point2f((float) jni_arr[0], (float)jni_arr[1]));
-    imagePoint.push_back(Point2f((float) jni_arr[2], (float)jni_arr[3]));
-    imagePoint.push_back(Point2f((float) jni_arr[4], (float)jni_arr[5]));
-    imagePoint.push_back(Point2f((float) jni_arr[6], (float)jni_arr[7]));
-
-//    -   With @ref SOLVEPNP_IPPE_SQUARE this is a special case suitable for marker pose estimation.
-//            Number of input points must be 4. Object points must be defined in the following order:
+//    Number of input points must be 4. Object points must be defined in the following order:
 //    - point 0: [-squareLength / 2,  squareLength / 2, 0]
 //    - point 1: [ squareLength / 2,  squareLength / 2, 0]
 //    - point 2: [ squareLength / 2, -squareLength / 2, 0]
 //    - point 3: [-squareLength / 2, -squareLength / 2, 0]
-
+    vector<Point3f> objectPoint;
     float squareLength = 1;
-
-    objectPoint.push_back(Point3f(-squareLength/2, squareLength / 2, 0));
-    objectPoint.push_back(Point3f(squareLength/2, squareLength / 2, 0));
-    objectPoint.push_back(Point3f(squareLength/2, -squareLength / 2, 0));
-    objectPoint.push_back(Point3f(-squareLength/2, -squareLength / 2, 0));
-
-    Mat rvecs, tvecs; // 카메라 rotation, translation
+    objectPoint.emplace_back(-squareLength/2, squareLength / 2, 0);
+    objectPoint.emplace_back(squareLength/2, squareLength / 2, 0);
+    objectPoint.emplace_back(squareLength/2, -squareLength / 2, 0);
+    objectPoint.emplace_back(-squareLength/2, -squareLength / 2, 0);
 
     // 카메라 rotation과 translation 벡터 찾기
+    // @ref SOLVEPNP_IPPE_SQUARE this is a special case suitable for marker pose estimation.
+    Mat rvecs, tvecs; // 카메라 rotation, translation
     solvePnP(objectPoint, imagePoint,cameraM, distortionC, rvecs, tvecs, false, SOLVEPNP_IPPE_SQUARE);
-    vector<cv::Point3f> obj_pts;
-
-    for(int i = 0; i < len / 3; i++)
-    {
-        obj_pts.push_back(Point3f((float)jni_draw_array[3*i], (float)jni_draw_array[3*i+1], (float)jni_draw_array[3*i+2]));
-    }
 
     // 3D 포인터를 이미지 평면에 투영
+    vector<cv::Point3f> obj_pts;
+    for(int i = 0; i < len / 3; i++) {
+        obj_pts.emplace_back((float)jni_draw_array[3*i, (float)jni_draw_array[3*i + 1], (float)jni_draw_array[3*i + 2]);
+    }
     projectPoints(obj_pts, rvecs, tvecs, cameraM, distortionC, imagePoint);
 
-    std::vector<Point2i> vector_pts;
-    for(Point_<float> & i : imagePoint)
-    {
+    vector<Point2i> vector_pts;
+    // 이미지 평면에 투영 시킨 점들을 가지고 polyline을 그립니다.
+    for(Point_<float> & i : imagePoint) {
         vector_pts.push_back(i);
     }
-
     polylines(matInput, vector_pts, true, Scalar(255.0, 0.0, 0.0), 20);
 
     (*env).ReleaseDoubleArrayElements(arr, jni_arr, 0);
     (*env).ReleaseDoubleArrayElements(drawArray, jni_draw_array, 0);
-}
-
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_example_apriltagapp_view_camera_CameraFragment_DrawArrow2(JNIEnv *env, jobject thiz,
-                                                                  jlong mat_addr_input,
-                                                                  jdoubleArray arr,
-                                                                  jlong camera_matrix,
-                                                                  jlong distortion_coefficients) {
-    Mat &matInput = *(Mat *)mat_addr_input;
-    jdouble* arr2 = (*env).GetDoubleArrayElements( arr, NULL);
-    Mat &cameraM = *(Mat *)camera_matrix;
-    Mat &distortionC = *(Mat *)distortion_coefficients;
-
-    vector<Point3f> objectPoint;
-    vector<Point2f> imagePoint;
-
-
-    imagePoint.push_back(Point2f((float) arr2[0], (float)arr2[1]));
-    imagePoint.push_back(Point2f((float) arr2[2], (float)arr2[3]));
-    imagePoint.push_back(Point2f((float) arr2[4], (float)arr2[5]));
-    imagePoint.push_back(Point2f((float) arr2[6], (float)arr2[7]));
-
-    Mat rvecs, tvecs;
-
-    float squareLength = 1;
-
-    objectPoint.push_back(Point3f(-squareLength/2, squareLength / 2, 0));
-    objectPoint.push_back(Point3f(squareLength/2, squareLength / 2, 0));
-    objectPoint.push_back(Point3f(squareLength/2, -squareLength / 2, 0));
-    objectPoint.push_back(Point3f(-squareLength/2, -squareLength / 2, 0));
-
-
-    solvePnP(objectPoint, imagePoint,cameraM, distortionC, rvecs, tvecs, false, SOLVEPNP_IPPE_SQUARE);
-    vector<cv::Point3f> obj_pts;
-
-    obj_pts.emplace_back(-1.2, 0, -0.3);
-    obj_pts.emplace_back(-1.2, 0, -4.3);
-    obj_pts.emplace_back(-1.7, 0, -4.3);
-    obj_pts.emplace_back(0, 0, -5.3);
-    obj_pts.emplace_back(1.7, 0, -4.3);
-    obj_pts.emplace_back(1.2, 0, -4.3);
-    obj_pts.emplace_back(1.2, 0, -0.3);
-
-
-    projectPoints(obj_pts, rvecs, tvecs, cameraM, distortionC, imagePoint);
-
-    std::vector<Point2i> vector_pts;
-    vector_pts.push_back(imagePoint.at(0));
-    vector_pts.push_back(imagePoint.at(1));
-    vector_pts.push_back(imagePoint.at(2));
-    vector_pts.push_back(imagePoint.at(3));
-    vector_pts.push_back(imagePoint.at(4));
-    vector_pts.push_back(imagePoint.at(5));
-    vector_pts.push_back(imagePoint.at(6));
-    polylines(matInput, vector_pts, true, Scalar(255.0, 0.0, 0.0), 30);
 }
--- sdk/java/src/apriltag/ApriltagDrawNative.java
+++
@@ -1,8 +0,0 @@
-package apriltag;
-
-public class ApriltagDrawNative {
-
-    public static native void DrawArrow(long matAddrInput, double[] arr, double[] arrowRight);
-
-
-}
