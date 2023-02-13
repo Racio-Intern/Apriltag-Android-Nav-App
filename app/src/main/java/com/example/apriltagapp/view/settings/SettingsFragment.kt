ac4fe74df40a6d9220e5d0caf7e48419f655706e
hhtboy <59467684+hhtboy@users.noreply.github.com>
2023-02-13 16:58:02 +0900

    decimate factor, thread, tag family 설정하도록 변경

--- app/src/main/java/com/example/apriltagapp/view/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/apriltagapp/view/settings/SettingsFragment.kt
@@ -1,60 +1,95 @@
 package com.example.apriltagapp.view.settings
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import apriltag.ApriltagNative
 import com.example.apriltagapp.R
+import com.example.apriltagapp.databinding.FragmentSearchBinding
+import com.example.apriltagapp.databinding.FragmentSettingsBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SettingsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var binding: FragmentSettingsBinding? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    val deci: Array<String> = arrayOf("1", "1.5", "2", "3", "4")
+    val threads: Array<String> = arrayOf("0", "2", "4", "6", "8")
+    val tagFamilies: Array<String> = arrayOf("tag16h5" ,"tag25h9","tag36h10" ,"tag36h11",  "tagStandard41h12")
+
+    var decimateFactor = 4.0
+    var tagFamily = "tagStandard41h12"
+    var thread = 1
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentSettingsBinding.inflate(inflater)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        binding?.btnDecimation?.setOnClickListener {
+            val builder =
+                AlertDialog.Builder(this.activity)
+            builder.setTitle("Select Station").setItems(
+                deci
+            ) { _, which ->
+                Toast.makeText(
+                    this.activity,
+                    "",
+                    Toast.LENGTH_SHORT
+                ).show()
+                decimateFactor = deci[which].toDouble()
+
+                ApriltagNative.apriltag_init(tagFamily, 2, decimateFactor, 0.0, thread)
+            }.show()
+
+
+        }
+
+        binding?.btnThreads?.setOnClickListener {
+            val builder =
+                AlertDialog.Builder(this.activity)
+            builder.setTitle("Select Station").setItems(
+                threads
+            ) { _, which ->
+                Toast.makeText(
+                    this.activity,
+                    "",
+                    Toast.LENGTH_SHORT
+                ).show()
+                thread = threads[which].toInt()
+                ApriltagNative.apriltag_init(tagFamily, 2, decimateFactor, 0.0, thread)
+            }.show()
+
+
+        }
+        binding?.btnTagFamily?.setOnClickListener {
+            val builder =
+                AlertDialog.Builder(this.activity)
+            builder.setTitle("Select Station").setItems(
+                tagFamilies
+            ) { _, which ->
+                Toast.makeText(
+                    this.activity,
+                    "",
+                    Toast.LENGTH_SHORT
+                ).show()
+                tagFamily = tagFamilies[which]
+                ApriltagNative.apriltag_init(tagFamily, 2, decimateFactor, 0.0, thread)
+            }.show()
+
+
+        }
+        return binding?.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SettingsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
