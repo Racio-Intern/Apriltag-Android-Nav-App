97a980f274d9014875982713db52144959f16b5f
Serenata2 <astg98@gmail.com>
2023-02-22 11:23:10 +0900

    실시간 cameraCalibrate을 통한 pos 추정하는 함수 구현 -> 상당히 부정확함

--- sdk/java/src/apriltag/OpenCVNative.java
+++ sdk/java/src/apriltag/OpenCVNative.java
@@ -10,7 +10,7 @@ public class OpenCVNative {
 
     public static native double[] apriltag_detect_and_pos_estimate(long matAddrInput, double[] arr, double[] cameraMatrixData);
 
-    public static native void calibrateCamera(long matAddrInput, double[] arr, int[] imageSize);
+    public static native double[] calibrateCamera(long matAddrInput, double[] arr, int[] imageSize);
 
     public static native double[] find_camera_focal_length(double[] arr, int[] imageSize);
 }
